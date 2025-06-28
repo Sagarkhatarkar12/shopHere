7aca28cfe238948db0247efd7eaa2e1e21bff79e
SDB <91771020+Sagarkhatarkar12@users.noreply.github.com>
2025-06-28 08:23:45 +0530

    all product page created

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -1,30 +1,58 @@
 <template>
-<div class="flex flex-col  w-70 h-fit overflow-hidden ">
+    <div class="flex flex-col flex-shrink-0 w-80 h-120 hover:shadow-2xl hover:border-2 border-black hover:bg-gray-200 cursor-pointer rounded-2xl">
 
-    <div class="flex w-fit">
-        <div class="h-full  rounded-2xl overflow-hidden">
+        <div class="flex w-full h-[65%]  ">
+            <div class="h-full w-full rounded-2xl overflow-hidden">
 
-            <img src="../assets/images.jpeg" alt="">
+                <img class="w-full h-full" :src="imageUrl" alt=""> 
+            </div>
+        </div>
+        <div class="flex justify-between h-[35%] py-2 px-1">
+            <div class="flex flex-col gap-1.5">
+                <h1 class="font-bold">{{ prouductName }}</h1>
+                <p class="text-sm font-bold text-gray-600">{{ brandName }}</p>
+                <div class="text-green-700 gap-1">
+                  
+                  <i v-for="n in star" :key="'fill' + n" class="ri-star-fill"></i>
+                   <i v-for="n in 5 - star" :key="'line' + n" class="ri-star-line"></i>
+
+                </div>
+                <button
+                    class="h-8  w-fit px-3 cursor-pointer rounded-full border-2 border-black  hover:bg-green-600 hover:border-none hover:text-white">Add
+                    to cart</button>
+            </div>
+            <div class="flex flex-col">
+                <p class="font-bold">${{ amoundValue }}</p>
+            </div>
         </div>
     </div>
-    <div class="flex justify-between py-1">
-<div class="flex flex-col gap-1.5">
-<h1 class="font-bold">Wireless Earbud, IPX8</h1>
-<p class="text-sm font-bold text-gray-600">organ cateogry , fairtrade certified</p>
-<div class="text-green-700 gap-1">
-    <i class="ri-star-line"></i>
-    <i class="ri-star-line"></i>
-    <i class="ri-star-line"></i>
-    <i class="ri-star-fill"></i>
-    <i class="ri-star-fill"></i>
-</div>
-<button class="h-8  w-fit px-3 cursor-pointer rounded-full border-2 border-black  hover:bg-green-600 hover:text-white" >Add to cart</button>
-</div>
-<div class="flex flex-col">
-    <p class="font-bold">$99</p>
-</div>
-    </div>
-</div>
 </template>
 <script>
+export default {
+    props: {
+        imageUrl:{
+            type:String,
+          required: true
+        },
+        prouductName: {
+            type: String,
+            requried: true
+        }
+        ,
+        brandName: {
+            type: String,
+            requried: true
+        },
+        star:{
+         type:Number,
+         requried:true
+        }
+        ,amoundValue:{
+            type:Number,
+            requried:true
+        }
+
+
+    }
+}
 </script>
--- src/components/CardContainer.vue
+++ src/components/CardContainer.vue
@@ -1,29 +1,63 @@
 <template>
     <div>
-        <div class="flex px-10 "> 
-            <h1 class="py-3 text-3xl font-sans px-2 font-bold">HeadPhone for you !</h1>
+        <div class="flex px-10 ">
+            <h1 class="py-3 text-3xl  font-sans px-2 font-bold">HeadPhone for you !</h1>
         </div>
-        <div class="flex h-fit flex-wrap px-10 py-10 gap-10 justify-center">
-<card/>
-<card/>
-<card/>
-<card/>
-<card/>
-<card/>
-<card/>
-<card/>
-<card/>
-<card/>
+        <div class="flex  h-full w-360 mx-10 my-10 gap-10 py-10 custom-scrollbar overflow-scroll">
+
+      <card v-for="item in products" :key = item.id :prouductName="item.title" :brandName="item.brand" :star="Math.round(item.rating)" :amoundValue="item.price"    :imageUrl="item.thumbnail" />
+    
+
+ 
+     
         </div>
     </div>
 </template>
 
 <script>
 import card from "./Card.vue"
-export default{
-components:{
-    card
-}
+export default {
+
+    components: {
+        card
+    },
+    data(){
+           return {
+      products: []
+    }
+  },
+  mounted(){
+    fetch("https://dummyjson.com/products")
+      .then((res) => res.json())
+      .then((data) => {
+        this.products = data.products;
+        console.log("Fetched products:", this.products);
+      })
+  }
+  
+  
+
 }
 
+
+
 </script>
+<style>
+/* styles.css or inside a <style> block */
+.custom-scrollbar::-webkit-scrollbar {
+  width: 8px;
+}
+.custom-scrollbar::-webkit-scrollbar-track {
+  background: white;
+  border-radius: 10px;
+}
+.custom-scrollbar::-webkit-scrollbar-thumb {
+  background-color: gray; /* green-400 */
+  border-radius: 20px;
+  min-height: 50px;
+  width: 10px;
+  height: 10px;
+  border: 2px solid transparent;
+}
+
+</style>
